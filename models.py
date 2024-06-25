3bc182ce68eea092be7906b829350182fea97473
Burrito <37681398+burritosoftware@users.noreply.github.com>
2024-06-24 22:49:15 -0700

    Add OIDC support

--- models.py
+++ models.py
@@ -1,28 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import func
 
 db = SQLAlchemy()
-login = LoginManager()
-
-
-@login.user_loader
-def load_user(_id):
-    return User.query.get(int(_id))
-
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True, unique=True)
-    username = db.Column(db.String(100))
-    password_hash = db.Column(db.String)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
 
 class Videos(db.Model):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True, unique=True)
--- thegateway/form.py
+++ thegateway/form.py
@@ -3,14 +3,10 @@ from flask_wtf.file import FileRequired
 from wtforms import (
     StringField,
     SubmitField,
-    PasswordField,
     FileField,
     SelectField,
-    TextAreaField,
-    BooleanField,
-    IntegerField,
 )
-from wtforms.validators import DataRequired, Length, ValidationError
+from wtforms.validators import DataRequired, Length
 import enum
 
 
@@ -29,40 +25,6 @@ class VideoType(enum.Enum):
     @classmethod
     def choices(cls):
         return [(choice.value, choice.name) for choice in cls]
-
-class LoginForm(FlaskForm):
-    username = StringField("Username")
-    password = PasswordField("Password")
-    submit = SubmitField("Enter the gateway")
-
-
-class NewUserForm(FlaskForm):
-    username = StringField("Username", validators=[DataRequired()])
-    password1 = PasswordField("Password", validators=[DataRequired()])
-    password2 = PasswordField("Confirm Password", validators=[DataRequired()])
-    upload = SubmitField("Complete")
-
-    def validate_password1(self, _):
-        if self.password1.data != self.password2.data:
-            return ValidationError("New passwords must be the same")
-
-
-class ChangePasswordForm(FlaskForm):
-    current_password = PasswordField("Password", validators=[DataRequired()])
-    new_password = PasswordField("New Password", validators=[DataRequired()])
-    new_password_confirmation = PasswordField(
-        "Confirm New Password", validators=[DataRequired()]
-    )
-    complete = SubmitField("Complete")
-
-    def validate_current_password(self, _):
-        if self.current_password.data == self.new_password.data:
-            return ValidationError("New password cannot be the same as current!")
-
-    def validate_new_password(self, _):
-        if self.new_password.data != self.new_password_confirmation.data:
-            return ValidationError("New passwords must be the same")
-
 
 class VideoForm(FlaskForm):
     video = FileField("Video", validators=[FileRequired()])
